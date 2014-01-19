51be2b20424353b264dd3449537d8254cb9391c9
kpmoy88 <kpmoy88@gmail.com>
2014-01-19 13:38:01 -0500

    Updated Answer

--- 04 - Spiral/lib/array.rb
+++ 04 - Spiral/lib/array.rb
@@ -42,15 +42,6 @@ class Array
   				outer_array_num-=1
   			end
   		end
-  		# if(outer_array_num>=inner_array_num && outer_array_num==self[outer_array_num].length-1)
-  		#  	inner_array_num-=1
-  		# elsif(inner_array_num>outer_array_num && inner_array_num==self[outer_array_num].length-1)#Goes to bottom of current column
-  		# 	outer_array_num+=1
-  		# elsif(inner_array_num<self[outer_array_num].length)#Goes to the end of the current top row
-  		# 	inner_array_num+=1
-  		# elsif(outer_array_num>inner_array_num)
-  		# 	outer_array_num-=1
-  		# end
   	end
   	spiral_array
   end
