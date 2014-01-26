9431a60df84705d62e806094497b833931808d9e
kpmoy88 <kpmoy88@gmail.com>
2014-01-26 03:41:15 -0500

    Updated Answer

--- 03 - Prime/lib/prime.rb
+++ 03 - Prime/lib/prime.rb
@@ -2,10 +2,6 @@ require_relative 'fixnum'
 
 class Prime
   def self.calculate(n)
-    # View README.md for instructions
-    # Hint: 
-    #  - Use the prime? method you created in prime.rb
-    #  - You only need to see if the first 1000 numbers are prime
   	primes = Array.new
   	cur_n = 0
   	cur_num = 2
@@ -16,6 +12,6 @@ class Prime
   		end
   		cur_num+=1
   	end
-  	return primes[n-1]
+  	primes[n-1]
   end
 end
